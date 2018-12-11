d69cbe9efbdb1bc6a2d515702dbcfbb002aab37f
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2018-12-11 21:35:50 +0900

    set typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <h1>freks blog</h1>
+    <div>Hello world!</div>
+
+    <div>
+      <h3>first title</h3>
+      <small>2018-12-11</small>
+      <div>sample</div>
+    </div>
+  </div>
+)
