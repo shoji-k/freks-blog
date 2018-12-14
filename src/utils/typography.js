d4a5adfaddb585e9efb6157de2b826357facd016
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2018-12-14 15:11:12 +0900

    install ocean beach theme and add jp font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,14 @@
 import Typography from 'typography'
+import theme from 'typography-theme-ocean-beach'
+theme.googleFonts.push(
+  {
+    name: 'Noto+Sans+JP',
+    styles: ['400'],
+  }
+)
+theme.bodyFontFamily = ['Noto Sans JP', 'Roboto', 'serif']
 
-const typography = new Typography({ baseFontSize: '24px' })
+const typography = new Typography(theme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
