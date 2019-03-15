c7ed649f392b3bd0ecc770796e5609f0de6f6cf5
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2019-03-15 17:17:36 +0900

    filter future date in rss, fixes #39

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,61 @@ module.exports = {
         }`,
       },
     },
-    'gatsby-plugin-feed',
-    'gatsby-plugin-draft',
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  // custom_elements: [{ 'content:encoded': edge.node.html }],
+                })
+              })
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+                filter: { fields: { draft: { eq: false } } }
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: '/rss.xml',
+            title: 'freks blog RSS Feed',
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-draft', // can hide future date post
   ],
 }
