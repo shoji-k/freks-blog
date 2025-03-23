c9aa07042046f75c9f4fe184702150b51996b358
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2025-03-23 17:30:56 +0900

    remove StaticQuery

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,46 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 export function Seo({ description, meta = [], keywords = [], title }) {
-  return (
-    <StaticQuery
-      query={detailsQuery}
-      render={(data) => {
-        const titleTemplate =
-          title === data.site.siteMetadata.title
-            ? title
-            : `%s | ${data.site.siteMetadata.title}`
-        const metaDescription =
-          description || data.site.siteMetadata.description
-        const siteUrl = data.site.siteMetadata.siteUrl || 'https://blog.freks.jp'
-        const author = 'kobayashi_shoji'
+  const data = useStaticQuery(detailsQuery)
+  const titleTemplate =
+    title === data.site.siteMetadata.title
+      ? title
+      : `%s | ${data.site.siteMetadata.title}`
+  const metaDescription =
+    description || data.site.siteMetadata.description
+  const siteUrl = data.site.siteMetadata.siteUrl || 'https://blog.freks.jp'
+  const author = 'kobayashi_shoji'
 
-        return (
-          <>
-            <title>{titleTemplate}</title>
-            <meta name="description" content={metaDescription} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={metaDescription} />
-            <meta property="og:type" content="website" />
-            <meta property="og:image" content={`${siteUrl}/logo.png`} />
-            <meta property="og:image:width" content="498" />
-            <meta property="og:image:height" content="484" />
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:creator" content={author} />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={metaDescription} />
-            <meta property="twitter:image" content={`${siteUrl}/logo.png`} />
-            {keywords.length > 0 && (
-              <meta name="keywords" content={keywords.join(', ')} />
-            )}
-            {meta.map((m, i) => (
-              <meta key={i} {...m} />
-            ))}
-          </>
-        )
-      }}
-    />
+  return (
+    <>
+      <title>{titleTemplate}</title>
+      <meta name="description" content={metaDescription} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={`${siteUrl}/logo.png`} />
+      <meta property="og:image:width" content="498" />
+      <meta property="og:image:height" content="484" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content={author} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={metaDescription} />
+      <meta property="twitter:image" content={`${siteUrl}/logo.png`} />
+      {keywords.length > 0 && (
+        <meta name="keywords" content={keywords.join(', ')} />
+      )}
+      {meta.map((m, i) => (
+        <meta key={i} {...m} />
+      ))}
+    </>
   )
 }
 
